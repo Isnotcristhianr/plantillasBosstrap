449b63ea0468234a4f8897b4346cc1e6a3b2af26
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2023-11-15 19:50:46 -0500

    login e inicio implementado

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,5 +10,9 @@ $routes->get('/', 'Home::index');
 //blank
 $routes->get('/blank', 'Home::Home');
 
+
 //login
-$routes->get('/loginadmin', 'ControllerAdmin\loginController::loginadmin');
+$routes->get('/login', 'ControllerAdmin\loginController::login');
+
+//inicio
+$routes->get('/admin', 'ControllerAdmin\loginController::inicio');
--- app/Controllers/ControllerAdmin/loginController.php
+++ app/Controllers/ControllerAdmin/loginController.php
@@ -8,17 +8,28 @@ use CodeIgniter\Controller;
 
 class loginController extends BaseController
 {
-    public function loginadmin(): string
+
+    public function login(): string
     {
         //header
         return view('layoutAdmin/header')
-        //sidebar
-        . view('layoutAdmin/sidebar')
-        //topbar
-        . view('layoutAdmin/topbar')
-        //content
-        . view('layoutAdmin/content')
-        //footer
-        . view('layoutAdmin/footer');
+            //login
+            . view('FormLogin')
+            //footer
+            . view('layoutAdmin/footer');
+    }
+
+    public function inicio(): string
+    {
+        //header
+        return view('layoutAdmin/header')
+            //sidebar
+            . view('layoutAdmin/sidebar')
+            //topbar
+            . view('layoutAdmin/topbar')
+            //content
+            . view('layoutAdmin/content')
+            //footer
+            . view('layoutAdmin/footer');
     }
 }
